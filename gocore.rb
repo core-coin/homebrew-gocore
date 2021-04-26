e924a2445fde62f4cdd087c2e51fb4ba7a9f330b
error2215 <slyvkamm@gmail.com>
2021-04-26 20:49:42 +0300

    gocore formula

---
+++ gocore.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Gocore< Formula
+  desc "Official Golang implementation of the CORE protocol."
+  homepage "https://coreblockchain.cc/"
+  url "https://github.com/core-coin/go-core/archive/v1.0.18-dev.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "aedede9bcffe74858a8825dfaf8dfcaf8c2b39b528ad0911d51e96500ec01158"
+  license "LGPL-3.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "make", "all"
+    bin.install Dir["build/bin"]
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test go-ethereum`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
