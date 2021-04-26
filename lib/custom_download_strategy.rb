e14d80425175fb4fd26c6897dbe65412b8f8d7bc
error2215 <slyvkamm@gmail.com>
2021-04-27 00:55:35 +0300

    gocore formula

--- lib/custom_download_strategy.rb
+++ lib/custom_download_strategy.rb
@@ -27,7 +27,7 @@ class CustomGitHubPrivateRepositoryDownloadStrategy < CurlDownloadStrategy
   end
 
   def download_url
-    "https://github.com/#{@owner}/#{@repo}/#{@filepath}"
+    "https://github.com/core-coin/go-core/archive/refs/tags/v1.0.18-dev.tar.gz"
   end
 
   private
