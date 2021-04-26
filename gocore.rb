3a2e302573ad9718ba02b42a8767bf50e937d88e
error2215 <slyvkamm@gmail.com>
2021-04-26 22:19:12 +0300

    gocore formula

--- gocore.rb
+++ gocore.rb
@@ -4,7 +4,8 @@
 class Gocore< Formula
   desc "Official Golang implementation of the CORE protocol."
   homepage "https://coreblockchain.cc/"
-  url "https://github.com/core-coin/go-core/archive/v1.0.18-dev.tar.gz.git", :using => GitHubGitDownloadStrategy
+  url "https://github.com/core-coin/go-core.git", :using => GitHubGitDownloadStrategy
+  version "1.0.18-dev"
   sha256 "aedede9bcffe74858a8825dfaf8dfcaf8c2b39b528ad0911d51e96500ec01158"
   license "LGPL-3.0"
 
