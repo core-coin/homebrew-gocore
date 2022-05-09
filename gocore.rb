1875783229189f75b0aecaca301542b3bcd78a92
error2215 <slyvkamm@gmail.com>
2022-05-09 13:57:24 +0200

    update formula

--- gocore.rb
+++ gocore.rb
@@ -1,21 +1,15 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-require_relative "lib/custom_download_strategy"
-
 class Gocore< Formula
   desc "Official Golang implementation of the CORE protocol."
   homepage "https://coreblockchain.cc/"
-  url "https://github.com/core-coin/go-core/v1.0.44-dev.tar.gz", :using => CustomGitHubPrivateRepositoryDownloadStrategy
-  version "1.0.44-dev"
-  sha256 "bc0147190ef50b8c3207edea2f03504e844f9002420b43e2166e0d65fbf70e9a"
-  license "LGPL-3.0"
+  url "https://github.com/core-coin/go-core/archive/refs/tags/v1.1.4.tar.gz",
+  version "1.1.4"
+  sha256 "149c50ed8d03282bf9cddb3e146c9cfcf6dce0d709f3929d4d5adde586354e70"
+  license "CORE License"
 
-  # depends_on "cmake" => :build
   depends_on "go@1.17"
   
   def install
     system "make", "all"
-    bin.install Dir["build/bin/*"]
+    bin.install "build/bin/gocore"
   end
 end
